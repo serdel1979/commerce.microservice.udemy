6c46b00970ba25ac3d2b6d1085535fdfe0cab357
Sergio De Luca <sdlbsso@gmail.com>
2024-08-28 16:06:24 -0300

    microservice libros creado

---
+++ Comercio/Libros/Persistencia/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Libros.Modelo;
+using Microsoft.EntityFrameworkCore;
+
+namespace Libros.Persistencia
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+
+        public DbSet<LibreriaMaterial> LibreriaMaterials { get; set; }
+    }
+}
