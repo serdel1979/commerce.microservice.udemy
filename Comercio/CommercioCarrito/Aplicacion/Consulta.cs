476c8189963b45255c242146a7d2af8c50de05fe
sergio <sdlbsso@gmail.com>
2024-09-09 09:31:25 -0300

    prueba haciendo carrito

--- Comercio/CommercioCarrito/Aplicacion/Consulta.cs
+++ Comercio/CommercioCarrito/Aplicacion/Consulta.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using CommercioCarrito.DTOs;
 using CommercioCarrito.Persistencia;
+using CommercioCarrito.RemoteInterface;
+using CommercioCarrito.RemoteModel;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
@@ -20,20 +22,31 @@ namespace CommercioCarrito.Aplicacion
         {
             private readonly ApplicationDbContext _context;
             private readonly IMapper _mapper;
+            private readonly ILibroService _libroService;
 
-            public Manejador(ApplicationDbContext context, IMapper mapper)
+            public Manejador(ApplicationDbContext context, IMapper mapper,
+                ILibroService libroService)
             {
                 this._context = context;
                 this._mapper = mapper;
+                this._libroService = libroService;
             }
             public async Task<CarritoDTO> Handle(Ejecuta request, CancellationToken cancellationToken)
             {
                 //var car = _mapper.Map<CarritoDTO>(await _context.CarritoSesions.Where(c=>c.Id ==  request.CarritoId).Include(d=>d.ListaDetalle).FirstOrDefaultAsync());
+           
+                var car = await _context.CarritoSesions.Include(d=>d.ListaDetalle)
+                    .FirstOrDefaultAsync(c => c.Id == request.CarritoId);
 
-                               
-                var car = await _context.CarritoSesions.Where(c => c.Id == request.CarritoId)
-                    .Include(d=>d.ListaDetalle)
-                    .FirstOrDefaultAsync();
+                var detalleCarrito = await _context.CarritoSesionDetalles
+                    .Where(x => x.Id == request.CarritoId).ToListAsync();
+
+                foreach(var libro in detalleCarrito)
+                {
+                    await _libroService.GetLibro(libro.Id);
+
+                }
+
 
                 var cardto = _mapper.Map<CarritoDTO>(car);
                 return cardto;
