c6184e89a2d91b91dfe1de1f4b781e5a8a19357f
sergio <sdlbsso@gmail.com>
2024-09-10 11:52:17 -0300

    haciendo testing -min 14

--- Comercio/TestComercio/LibroServiceTest.cs
+++ Comercio/TestComercio/LibroServiceTest.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using Libros.Aplicacion;
+using Libros.DTOs;
 using Libros.Mapeo;
+using Libros.Modelo;
 using Libros.Persistencia;
 using Moq;
 using System;
@@ -8,11 +10,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GenFu;
+using Microsoft.EntityFrameworkCore;
 
 namespace TestComercio
 {
     public class LibroServiceTest
     {
+
+
+        private List<LibreriaMaterial> ObtenerDataPrueba()
+        {
+            A.Configure<LibreriaMaterial>()
+                 .Fill(x => x.Titulo).AsArticleTitle()
+                 .Fill(x => x.Id);
+
+            var lista = A.ListOf<LibreriaMaterial>(30);
+            lista[0].Id = 0;
+
+            return lista;
+
+        }
+
+        private Mock<LibreriaMaterial> CreaContexto()
+        {
+            var dataPrueba = ObtenerDataPrueba().AsQueryable();
+            var dbSet = new Mock<DbSet<LibreriaMaterial>>();
+
+            dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x=>x.Provider)
+                .Returns(dataPrueba.Provider);
+
+            dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x => x.Expression)
+                .Returns(dataPrueba.Expression);
+
+            dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x => x.ElementType)
+                     .Returns(dataPrueba.ElementType);
+
+            dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x => x.GetEnumerator())
+                .Returns(dataPrueba.GetEnumerator());
+
+
+
+        }
 
         [Fact]
         public void GetLibros()
@@ -35,6 +74,8 @@ namespace TestComercio
              */
 
             Consulta.Manejador handle = new Consulta.Manejador(mockContext.Object,mockMapper.Object);
+
+
 
         }
 
